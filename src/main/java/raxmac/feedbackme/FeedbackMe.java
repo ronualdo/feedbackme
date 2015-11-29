f9402071d12372edd4bb15e6cc3d5bedde02a365
ronualdo <raxmac@gmail.com>
2015-11-29 17:02:12 -0300

    change the way that we're passing database info to the app
    to allow the tests to run at other environments we're using
    environment variables to store database connection information

--- src/main/java/raxmac/feedbackme/FeedbackMe.java
+++ src/main/java/raxmac/feedbackme/FeedbackMe.java
@@ -2,12 +2,26 @@ package raxmac.feedbackme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@Configuration
 public class FeedbackMe {
 
     public static void main(String[] args) {
         SpringApplication.run(FeedbackMe.class, args);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
     }
 
 }
