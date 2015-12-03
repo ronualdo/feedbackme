88f44a7cdfdbe4ec8bddd7ca650e908c8509eace
ronualdo <raxmac@gmail.com>
2015-12-02 23:55:48 -0300

    include cors configuration

--- src/main/java/raxmac/feedbackme/FeedbackMe.java
+++ src/main/java/raxmac/feedbackme/FeedbackMe.java
@@ -6,6 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 
@@ -22,6 +25,16 @@ public class FeedbackMe {
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/{userName}/feedbacks").allowedOrigins("https://f33dbackmeweb.herokuapp.com");
+            }
+        };
     }
 
 }
